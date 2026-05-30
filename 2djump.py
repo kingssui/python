abe854af58d4d96f30bf1b4d62b82188ac9da053
kingssui <kingsdavidg@gmail.com>
2026-05-30 12:25:31 +0300

    Add files via upload
    https://docs.github.com/articles/using-pull-requests

--- 2djump.py
+++ 2djump.py
@@ -6,7 +6,15 @@ player=pygame.Rect(100,100,50,50)
 pltform=pygame.Rect(100,340,50,20)
 enemey=pygame.Rect(400,350,50,50)
 pltform2=pygame.Rect(300,340,50,20)
+pltform3=pygame.Rect(370,300,50,20)
+pltform4=pygame.Rect(600,300,50,20)
+pltform5=pygame.Rect(100,340,50,20)
+enemey1=pygame.Rect(400,100,50,50)
+pltform6=pygame.Rect(450,200,50,20)
+pltform7=pygame.Rect(200,200,50,20)
+pltform8=pygame.Rect(700,200,50,20)
 direction='right'
+enemey1_direction='down'
 px=0
 py=350
 jumptime=0
@@ -17,7 +25,7 @@ canjump=False
 #forevor blocf
 while True :
     player.x=px
-    if player.colliderect(pltform)or player.y>=350 or player.colliderect(pltform2):
+    if player.colliderect(pltform)or player.y>=350 or player.colliderect(pltform2)or player.colliderect (pltform3) or player.colliderect (pltform4)or player. colliderect(pltform5)or player.colliderect (pltform7) or player.colliderect (pltform8) :
         canjump=True
     else:
         canjump=False
@@ -26,6 +34,14 @@ while True :
     pygame.draw.rect(gamescreen,'blue',pltform)
     pygame.draw.rect(gamescreen,'green',enemey)
     pygame.draw.rect(gamescreen,'blue',pltform2)
+    pygame.draw.rect(gamescreen,'blue',pltform3)
+    pygame.draw.rect(gamescreen,'blue',pltform4)
+    pygame.draw.rect(gamescreen,'green',enemey1)
+    pygame.draw.rect(gamescreen,'green',pltform5)
+    
+    pygame.draw.rect(gamescreen,'green',pltform6)
+    pygame.draw.rect(gamescreen,'blue',pltform7)
+    pygame.draw.rect(gamescreen,'blue',pltform8)
     pygame.display.flip()
     buttons=pygame.event.get()
     for button in buttons:
@@ -55,17 +71,13 @@ while True :
         fallspeed = gravity * 0.64
         player.y+=0.64
     #check if we the pltform
-    if player.colliderect(pltform):
+    if player.colliderect(pltform)or player.colliderect(pltform2)or player.colliderect (pltform3) or player.colliderect (pltform4)or player.colliderect (pltform5)or player.colliderect (pltform6)or player.colliderect (pltform7)or player.colliderect (pltform8):
         print('pltform')
         isfalling=False
     else:
         isfalling=True
-        #pltfrom2
-    if player.colliderect(pltform2):
-        print('pltform2')
-        isfalling=False
-    else:
-        isfalling=True
+
+    
     #move the enemy
     if direction=="right":
         enemey.x+=1
@@ -75,4 +87,12 @@ while True :
         enemey.x-=1 
     if enemey.x<400:
         direction="right"
-        #
+        #move the enemy1
+    if  enemey1_direction=="down":
+        enemey1.y+=1
+    if enemey1.y>=150:
+      enemey1_direction="up"
+    if direction=="up":
+        enemey1.y-=1 
+    if enemey1.y<50:
+      enemey1_direction="down"
